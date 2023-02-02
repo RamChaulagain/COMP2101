9f9c3da87d697dd081240a9e92384cc73e91190e
Ram Chaulagain <ramchaulagain2018@gmail.com>
2023-02-01 19:41:46 -0500

    Lab 1

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "FQDN:$(hostname)"
-echo "Host Information:"
+
+echo "FQDN:$(hostname)" # echo is a basic command that prints text or string to the terminal or console
+echo "Host Information:" # host information isthe term refers to details about a device on the network, which can be retrieved using the command and includes information such as hostname, IP addresses, and DNS information, used for troubleshooting network issues and checking configurations
 hostnamectl status
-echo "IP Addresses:$(hostname -I)"
-df -h /boot/efi
+echo "ip address= $(hostname -i)"  #it shows ip address of the host
+df -h /boot/ef #df -h command in bash displays information about the disk space usage and availability of the file systems in a human-readable format
